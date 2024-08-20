58ccfd0a17e6cc3dd5c10cb86876f0315d4cc677
761jayanth8 <mahadevan3579vw@gmail.com>
2024-08-20 22:03:35 +0530

    Updates

--- precache-manifest.12e421826d130033fc164297d51d0782.js
+++ precache-manifest.12e421826d130033fc164297d51d0782.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e47a4a8e2a55dc53aa8e8f86a6a9313f",
+    "revision": "bdffe26fc490ac396cada939f5168d99",
     "url": "/AMZ-Clone/index.html"
   },
   {
-    "revision": "a42a7841afdb29f1cefd",
-    "url": "/AMZ-Clone/static/css/main.67260e6a.chunk.css"
+    "revision": "c47ea4122d41f75e34c9",
+    "url": "/AMZ-Clone/static/css/main.40298d81.chunk.css"
   },
   {
-    "revision": "cf0ea40974423d7d28e6",
-    "url": "/AMZ-Clone/static/js/2.2cc3cb18.chunk.js"
+    "revision": "1c45697d4bc30c8ee94e",
+    "url": "/AMZ-Clone/static/js/2.7058b343.chunk.js"
   },
   {
     "revision": "7dc1ecec1de9e5ffb651cfbfe0eefa33",
-    "url": "/AMZ-Clone/static/js/2.2cc3cb18.chunk.js.LICENSE.txt"
+    "url": "/AMZ-Clone/static/js/2.7058b343.chunk.js.LICENSE.txt"
   },
   {
     "revision": "be937cc31ddc31ef27d7",
     "url": "/AMZ-Clone/static/js/3.e21d1a54.chunk.js"
   },
   {
-    "revision": "a42a7841afdb29f1cefd",
-    "url": "/AMZ-Clone/static/js/main.9a44f7ad.chunk.js"
+    "revision": "c47ea4122d41f75e34c9",
+    "url": "/AMZ-Clone/static/js/main.ce573318.chunk.js"
   },
   {
     "revision": "31264a1970861521b302",
