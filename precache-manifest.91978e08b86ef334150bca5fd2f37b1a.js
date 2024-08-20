d9ad2def155b63bf4ad30f131b2dfe4383e4e9be
761jayanth8 <mahadevan3579vw@gmail.com>
2024-08-20 21:15:42 +0530

    Updates

---
+++ precache-manifest.91978e08b86ef334150bca5fd2f37b1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55da58f3a611c7c4fcb408388177a49",
+    "url": "/AMZ-Clone/index.html"
+  },
+  {
+    "revision": "5383b50de869dbf3a1bd",
+    "url": "/AMZ-Clone/static/css/main.67260e6a.chunk.css"
+  },
+  {
+    "revision": "cf0ea40974423d7d28e6",
+    "url": "/AMZ-Clone/static/js/2.2cc3cb18.chunk.js"
+  },
+  {
+    "revision": "7dc1ecec1de9e5ffb651cfbfe0eefa33",
+    "url": "/AMZ-Clone/static/js/2.2cc3cb18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be937cc31ddc31ef27d7",
+    "url": "/AMZ-Clone/static/js/3.e21d1a54.chunk.js"
+  },
+  {
+    "revision": "5383b50de869dbf3a1bd",
+    "url": "/AMZ-Clone/static/js/main.e02d015a.chunk.js"
+  },
+  {
+    "revision": "31264a1970861521b302",
+    "url": "/AMZ-Clone/static/js/runtime-main.99f8ae50.js"
+  }
+]);
